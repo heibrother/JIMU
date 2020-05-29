fc465798a87b48e8c61186f4ce5a17228eb127ec
connortang <connortang@etekcity.com.cn>
2020-05-29 16:08:16 +0800

    运行环境修改

--- sharecomponent/src/main/runalone/java/com.luojilab.share.runalone/application/ShareApplication.java
+++ sharecomponent/src/main/runalone/java/com.luojilab.share.runalone/application/ShareApplication.java
@@ -2,6 +2,8 @@ package com.luojilab.share.runalone.application;
 
 import android.app.Application;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 /**
  * Created by mrzhang on 2017/8/16.
  */
